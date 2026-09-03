7c1804eee19be655b93c55a499ece85cd465dfc1
Xiaoyu <xiaoyu.xy01.yang@gmail.com>
2026-09-04 01:04:42 +0200

    init the py fastapi

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+import Anthropic from "@anthropic-ai/sdk"
+import "dotenv/config"
+
+
+const myApiKey = process.env.AI_GATEWAY_API_KEY
+
+const client = new Anthropic({
+  apiKey: myApiKey,
+  baseURL: "https://ai-gateway.vercel.sh",
+})
